b6c684e5c4d31a4fa83ba9d32708ba86fc33a584
Elmer Nocon <elmernocon@gmail.com>
2025-03-24 08:42:25 +0800

    refactor(app): update app

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,32 +1,50 @@
-# Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+# Copyright (c) 2025 AccelByte Inc. All Rights Reserved.
 # This is licensed software from AccelByte Inc, for limitations
 # and restrictions contact your company contract manager.
 
 import asyncio
 import logging
+from logging import Logger
+from typing import List, Optional
 
-from app.proto.revocation_pb2_grpc import add_RevocationServicer_to_server
+from environs import Env
 
-from accelbyte_grpc_plugin import App, AppGRPCInterceptorOpt, AppGRPCServiceOpt
-from accelbyte_grpc_plugin.interceptors.authorization import (
-    AuthorizationServerInterceptor,
+from accelbyte_py_sdk.core import (
+    AccelByteSDK,
+    DictConfigRepository,
+    InMemoryTokenRepository,
+    HttpxHttpClient,
 )
-from accelbyte_grpc_plugin.interceptors.logging import (
-    DebugLoggingServerInterceptor,
-)
-from accelbyte_grpc_plugin.interceptors.metrics import (
-    MetricsServerInterceptor,
-)
-from accelbyte_grpc_plugin.opts.grpc_health_checking import GRPCHealthCheckingOpt
-from accelbyte_grpc_plugin.opts.grpc_reflection import GRPCReflectionOpt
-from accelbyte_grpc_plugin.opts.prometheus import PrometheusOpt
-from accelbyte_grpc_plugin.opts.zipkin import ZipkinOpt
+from accelbyte_py_sdk.services import auth as auth_service
 
-from accelbyte_grpc_plugin.utils import create_env, instrument_sdk_http_client
+from accelbyte_grpc_plugin import (
+    App,
+    AppOpt,
+    AppGRPCInterceptorOpt,
+    AppGRPCServiceOpt,
+)
+from accelbyte_grpc_plugin.utils import instrument_sdk_http_client
 
-from app.services.revocation_service import AsyncRevocationService
+from .proto.revocation_pb2_grpc import add_RevocationServicer_to_server
+from .services.revocation_service import AsyncRevocationService
+from .utils import create_env
 
 DEFAULT_APP_PORT: int = 6565
+
+DEFAULT_AB_BASE_URL: str = "https://test.accelbyte.io"
+DEFAULT_AB_NAMESPACE: str = "accelbyte"
+
+DEFAULT_ENABLE_HEALTH_CHECK: bool = True
+DEFAULT_ENABLE_PROMETHEUS: bool = True
+DEFAULT_ENABLE_REFLECTION: bool = True
+DEFAULT_ENABLE_ZIPKIN: bool = True
+
+DEFAULT_PLUGIN_GRPC_SERVER_AUTH_ENABLED: bool = True
+DEFAULT_PLUGIN_GRPC_SERVER_AUTH_RESOURCE: Optional[str] = None
+DEFAULT_PLUGIN_GRPC_SERVER_AUTH_ACTION: Optional[int] = None
+
+DEFAULT_PLUGIN_GRPC_SERVER_LOGGING_ENABLED: bool = False
+DEFAULT_PLUGIN_GRPC_SERVER_METRICS_ENABLED: bool = True
 
 
 async def main(**kwargs) -> None:
@@ -34,77 +52,126 @@ async def main(**kwargs) -> None:
 
     port: int = env.int("PORT", DEFAULT_APP_PORT)
 
-    opts = []
     logger = logging.getLogger("app")
     logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler())
 
+    config = DictConfigRepository(dict(env.dump()))
+    token = InMemoryTokenRepository()
+    http = HttpxHttpClient()
+    http.client.follow_redirects = True
+
+    sdk = AccelByteSDK()
+    sdk.initialize(
+        options={
+            "config": config,
+            "token": token,
+            "http": http,
+        }
+    )
+
+    instrument_sdk_http_client(sdk=sdk, logger=logger)
+
+    _, error = await auth_service.login_client_async(sdk=sdk)
+    if error:
+        raise Exception(str(error))
+
+    sdk.timer = auth_service.LoginClientTimer(2880, repeats=-1, autostart=True, sdk=sdk)
+
+    options = create_options(sdk=sdk, env=env, logger=logger)
+    options.append(
+        AppGRPCServiceOpt(
+            service=AsyncRevocationService(
+                sdk=sdk,
+                logger=logger,
+            ),
+            service_full_name=AsyncRevocationService.full_name,
+            add_service_func=add_RevocationServicer_to_server,
+        )
+    )
+
+    app = App(port=port, env=env, logger=logger, options=options)
+    await app.run()
+
+
+def create_options(sdk: AccelByteSDK, env: Env, logger: Logger) -> List[AppOpt]:
+    options: List[AppOpt] = []
+
     with env.prefixed("AB_"):
-        base_url = env("BASE_URL", "https://test.accelbyte.io")
-        client_id = env("CLIENT_ID", None)
-        client_secret = env("CLIENT_SECRET", None)
-        namespace = env("NAMESPACE", "accelbyte")
+        namespace = env.str("NAMESPACE", DEFAULT_AB_NAMESPACE)
 
-    with env.prefixed(prefix="ENABLE_"):
-        if env.bool("PROMETHEUS", True):
-            opts.append(PrometheusOpt())
-        if env.bool("HEALTH_CHECKING", True):
-            opts.append(GRPCHealthCheckingOpt())
-        if env.bool("REFLECTION", True):
-            opts.append(GRPCReflectionOpt())
-        if env.bool("ZIPKIN", True):
-            opts.append(ZipkinOpt())
+    with env.prefixed("ENABLE_"):
+        if env.bool("HEALTH_CHECK", env.bool("HEALTH_CHECKING", DEFAULT_ENABLE_HEALTH_CHECK)):
+            from accelbyte_grpc_plugin.opts.grpc_health_checking import (
+                GRPCHealthCheckingOpt,
+            )
 
-    with env.prefixed(prefix="PLUGIN_GRPC_SERVER_AUTH_"):
-        if env.bool("ENABLED", False):
-            from accelbyte_py_sdk import AccelByteSDK
-            from accelbyte_py_sdk.core import MyConfigRepository, InMemoryTokenRepository
-            from accelbyte_py_sdk.token_validation.caching import CachingTokenValidator
-            from accelbyte_py_sdk.services.auth import login_client, LoginClientTimer
+            options.append(GRPCHealthCheckingOpt())
+        if env.bool("PROMETHEUS", DEFAULT_ENABLE_PROMETHEUS):
+            from accelbyte_grpc_plugin.opts.prometheus import (
+                PrometheusOpt
+            )
 
-            config = MyConfigRepository(base_url, client_id, client_secret, namespace)
-            token = InMemoryTokenRepository()
+            options.append(PrometheusOpt())
+        if env.bool("REFLECTION", DEFAULT_ENABLE_REFLECTION):
+            from accelbyte_grpc_plugin.opts.grpc_reflection import (
+                GRPCReflectionOpt,
+            )
 
-            sdk = AccelByteSDK()
-            sdk.initialize(options={"config": config, "token": token})
+            options.append(GRPCReflectionOpt())
+        if env.bool("ZIPKIN", DEFAULT_ENABLE_ZIPKIN):
+            from accelbyte_grpc_plugin.opts.zipkin import (
+                ZipkinOpt
+            )
 
-            instrument_sdk_http_client(sdk=sdk, logger=logger)
+            options.append(ZipkinOpt())
 
-            result, error = login_client(sdk=sdk)
-            if error:
-                raise Exception(str(error))
+    with env.prefixed("PLUGIN_GRPC_SERVER_"):
+        with env.prefixed("AUTH_"):
+            if env.bool("ENABLED", DEFAULT_PLUGIN_GRPC_SERVER_AUTH_ENABLED):
+                from accelbyte_py_sdk.token_validation.caching import CachingTokenValidator
+                from accelbyte_grpc_plugin.interceptors.authorization import AuthorizationServerInterceptor
 
-            sdk.timer = LoginClientTimer(2880, repeats=-1, autostart=True, sdk=sdk)
-
-            opts.append(
-                AppGRPCInterceptorOpt(
-                    interceptor=AuthorizationServerInterceptor(
-                        namespace=namespace,
-                        token_validator=CachingTokenValidator(sdk=sdk),
+                options.append(
+                    AppGRPCInterceptorOpt(
+                        interceptor=AuthorizationServerInterceptor(
+                            token_validator=CachingTokenValidator(sdk=sdk),
+                            resource=env.str(
+                                "RESOURCE", DEFAULT_PLUGIN_GRPC_SERVER_AUTH_RESOURCE
+                            ),
+                            action=env.int(
+                                "ACTION", DEFAULT_PLUGIN_GRPC_SERVER_AUTH_ACTION
+                            ),
+                            namespace=namespace,
+                        )
                     )
+                )
+        if env.bool("LOGGING_ENABLED", DEFAULT_PLUGIN_GRPC_SERVER_LOGGING_ENABLED):
+            from accelbyte_grpc_plugin.interceptors.logging import (
+                DebugLoggingServerInterceptor,
+            )
+
+            options.append(
+                AppGRPCInterceptorOpt(
+                    interceptor=DebugLoggingServerInterceptor(logger=logger)
                 )
             )
 
-    if env.bool("PLUGIN_GRPC_SERVER_LOGGING_ENABLED", False):
-        opts.append(AppGRPCInterceptorOpt(DebugLoggingServerInterceptor(logger)))
+        if env.bool("METRICS_ENABLED", DEFAULT_PLUGIN_GRPC_SERVER_METRICS_ENABLED):
+            from accelbyte_grpc_plugin.interceptors.metrics import (
+                MetricsServerInterceptor,
+            )
 
-    if env.bool("PLUGIN_GRPC_SERVER_METRICS_ENABLED", True):
-        opts.append(AppGRPCInterceptorOpt(MetricsServerInterceptor()))
+            options.append(
+                AppGRPCInterceptorOpt(interceptor=MetricsServerInterceptor())
+            )
 
-    opts.append(
-        AppGRPCServiceOpt(
-            AsyncRevocationService(
-                logger=logger
-            ),
-            AsyncRevocationService.full_name,
-            add_RevocationServicer_to_server,
-        )
-    )
-    
-    logger.info("setup completed, running interceptors")
+    return options
 
-    await App(port, env, opts=opts).run()
+
+def run() -> None:
+    asyncio.run(main())
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    run()
--- src/app/services/revocation_service.py
+++ src/app/services/revocation_service.py
@@ -3,48 +3,51 @@
 # and restrictions contact your company contract manager.
 
 import json
-from logging import Logger, getLogger
+from logging import Logger
 from typing import Optional
 
 from google.protobuf.json_format import MessageToDict
 
-from app.proto.revocation_pb2 import (
+from accelbyte_py_sdk import AccelByteSDK
+
+from ..proto.revocation_pb2 import (
     RevokeRequest,
     RevokeResponse,
     DESCRIPTOR,
 )
+from ..proto.revocation_pb2_grpc import RevocationServicer
 
-from app.proto.revocation_pb2_grpc import RevocationServicer
+from ..object.revoke_entry_type import RevokeEntryType
+from ..object.revocation_status import RevocationStatus
+from ..revocation.revocations import Revocations
 
-from app.object.revoke_entry_type import RevokeEntryType
-from app.object.revocation_status import RevocationStatus
-from app.revocation.revocations import Revocations
 
 class AsyncRevocationService(RevocationServicer):
     full_name: str = DESCRIPTOR.services_by_name["Revocation"].full_name
 
-    def __init__(self, logger: Optional[Logger] = None) -> None:
+    def __init__(self, sdk: Optional[AccelByteSDK] = None, logger: Optional[Logger] = None) -> None:
+        self.sdk = sdk
         self.logger = logger
             
     async def Revoke(self, request: RevokeRequest, context):
         self.log_payload(f'{self.Revoke.__name__} request: %s', request)
-        response : RevokeResponse
+        response: RevokeResponse
         try:
             namespace = request.namespace
-            userId = request.userId
+            user_id = request.userId
             quantity = request.quantity
-
             revoke_entry_type = RevokeEntryType[request.revokeEntryType.upper()]
             revocation = Revocations().get_revocation(revoke_entry_type)
-            response = revocation.revoke(namespace, userId, quantity, request)
+            response = revocation.revoke(namespace, user_id, quantity, request)
         except Exception as e:
             response = RevokeResponse(
-                reason = f"Revocation method {str(e)} not supported",
-                status = RevocationStatus.FAIL.name,
+                reason=f"Revocation method {str(e)} not supported",
+                status=RevocationStatus.FAIL.name,
             )
         self.log_payload(f'{self.Revoke.__name__} response: %s', response)
         return response
-    
+
+    # noinspection PyShadowingBuiltins
     def log_payload(self, format : str, payload):
         if not self.logger:
             return
